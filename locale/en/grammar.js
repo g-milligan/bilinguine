5707ce988f07264530c755fe3b67aff740f44564
g-milligan <gmilligan@human-element.com>
2015-02-22 09:24:40 -0500

    initial commit

---
+++ locale/en/grammar.js
@@ -0,0 +1,94 @@
+//http://translate.google.com/translate_tts?tl=th&q=%E0%B9%80%E0%B8%82%E0%B9%89%E0%B8%B2
+
+function en_wordTypes(){
+  var r={
+    "name":"word types",
+    "summary":"All words belong to one or more categories. There are 9 of these category types.",
+    "keys":[
+      "noun","pronoun",
+      "adjective","verb",
+      "adverb","article",
+      "conjunction","preposition",
+      "interjection"
+    ],
+    "info":{
+      "1":{
+        "abbr":"n",
+        "def":"is a person, place, or thing",
+        "eg":["man","woman","America","Thailand","house","bike"],
+        "ico":""
+      },
+      "2":{
+        "abbr":"pron",
+        "def":"is a shorter substitute for a noun (person, place, or thing) that can be used instead of repeating the full noun multiple times",
+        "eg":["I","you","me","she","him","they","them","it"],
+        "ico":""
+      },
+      "3":{
+        "abbr":"adj",
+        "def":"describes qualities of a noun (person, place, or thing)",
+        "eg":["male","female","small","big","delicious"],
+        "ico":""
+      },
+      "4":{
+        "abbr":"v",
+        "def":"is an action, state, or occurence",
+        "eg":["run","learn","work","play","is","am"],
+        "ico":""
+      },
+      "5":{
+        "abbr":"adv",
+        "def":"describes qualities of a verb (action, state, or occurence)",
+        "eg":["quickly","efficiently","diligently","cheerfully","everywhere"],
+        "ico":""
+      },
+      "6":{
+        "abbr":"art",
+        "def":"indicate that a noun is a specific instance or occurence of a noun, by appearing before the noun",
+        "eg":["the","a","an"],
+        "ico":""
+      },
+      "7":{
+        "abbr":"conj",
+        "def":"can be used to join two complete sentences into one by describing a relationship between them",
+        "eg":["and","or","but","if","yet","nor","so","because"],
+        "ico":""
+      },
+      "8":{
+        "abbr":"prep",
+        "def":"describes a relative position, time, or similarity, between two or more things. Unlike a conjunction, a preposition does not join complete sentences into one.",
+        "eg":["before","to","under","over","in","like","of"],
+        "ico":""
+      },
+      "9":{
+        "abbr","interj",
+        "def":"is an exclamation used to capture attention or express a strong emotion",
+        "eg":["hey","ow","ugh"],
+        "ico":""
+      }
+    }
+  };
+  return r;
+}
+
+function en_sentenceParts(){
+  var r={
+    "name":"sentence parts",
+    "summary":"",
+    "keys":[
+      "subject","predicate","object"
+    ],
+    "info":{
+      "1":{
+        "def":""
+      },
+      "2":{
+        "def":""
+      },
+      "3":{
+        "def":""
+      }
+    }
+  };
+  return r;
+}
